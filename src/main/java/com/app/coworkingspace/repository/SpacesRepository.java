8903e26967dc7e90aa0a4ccc32dc8f34f18e5318
Alinur Mammadzada <138376201+AlinurMammadzada5@users.noreply.github.com>
2025-07-12 13:46:18 +0200

    Update SpacesRepository.java

--- src/main/java/com/app/coworkingspace/repository/SpacesRepository.java
+++ src/main/java/com/app/coworkingspace/repository/SpacesRepository.java
@@ -1,11 +1,1 @@
-package com.app.coworkingspace.repository;
 
-import com.app.coworkingspace.entity.Spaces;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SpacesRepository extends JpaRepository<Spaces, Integer> {
-    List<Spaces> findEmptyTables();
-    List<Spaces> findByUsername(String username);
-    long countByUsername(String username);
-}
